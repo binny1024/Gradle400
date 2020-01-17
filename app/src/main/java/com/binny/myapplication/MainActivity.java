a4d10afd8499842cf26afdc7c9b2d38d92671a63
binny1024 <596928539@qq.com>
2020-01-17 18:02:52 +0800

    garlde

---
+++ app/src/main/java/com/binny/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.binny.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void test(View view) {
+        String name = getString(R.string.name);
+        System.out.println("name = " + name);
+    }
+}
